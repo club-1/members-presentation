d8dff5c62fb1c33059eac0a1626c626a5d99aeca
Nicolas Peugnet <n.peugnet@free.fr>
2022-05-06 18:37:40 +0200

    make target work through iframe

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-$(".member").click(function(){
+$(".member").click(function () {
     var name = this.dataset.id;
-   $("#" + name).show();
- });
- 
- $(".member").draggable({ grid: [ 80, 80 ] });
- 
- 
- $(".presentation").draggable(function(){
-   handle: ".handle";
- });
- 
- $( "div, p" ).disableSelection();
- 
- 
- $(".content").resizable();
- 
- $(".close").click(function(){
-   $(this).parent().hide();
- });
+    $("#" + name).show();
+});
+
+$(".member").draggable({ grid: [80, 80] });
+
+
+$(".presentation").draggable(function () {
+    handle: ".handle";
+});
+
+$("div, p").disableSelection();
+
+
+$(".content").resizable();
+
+$(".close").click(function () {
+    $(this).parent().hide();
+});
+
+
+/**
+ * Manage messages comming into this window. 
+ * @param {MessageEvent} event The message received.
+ */
+function handleMessageEvent(event) {
+    if (!event.data.target) {
+        return;
+    }
+    let elem = document.getElementById(event.data.target);
+    if (!elem) {
+        return;
+    }
+    elem.style.display = 'block';
+}
+window.addEventListener('message', handleMessageEvent)
